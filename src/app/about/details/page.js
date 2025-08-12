291c300dd6993b767df087d59b5fd4af910d1ced
smabari <mdazizulbarishafin@gmail.com>
2025-08-13 02:54:50 +0600

    understanding  app routes

---
+++ src/app/about/details/page.js
@@ -0,0 +1,6 @@
+
+export default function detailsPage(){
+    return (
+        <div>Details Page</div>
+    )
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -22,6 +22,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <nav>Home | About | blog</nav>
         {children}
       </body>
     </html>
