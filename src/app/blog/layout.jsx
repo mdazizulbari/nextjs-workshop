4079266113e1a1951dab1c74da4e3904d62bb34d
smabari <mdazizulbarishafin@gmail.com>
2025-08-13 11:13:03 +0600

    more routing

---
+++ src/app/blog/layout.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function BlogLayout({ children }) {
+  return (
+    <>
+      <div className="">
+      </div>
+      <div>{children}</div>
+    </>
+  );
+}
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 export default function page() {
+  const blogs = [
+    {
+      id: 1,
+      title: "This is title",
+    },
+    {
+      id: 2,
+      title: "This is title 2",
+    },
+    {
+      id: 3,
+      title: "This is title 3",
+    },
+  ];
+
   return (
-    <div>Blog page</div>
-  )
+    <div>
+      {blogs.map((blog) => (
+        <div key={blog.id} className="border">
+          <h4 className="h-10">{blog.title}</h4>
+          <Link href={`blog/${blog.id}`}>details</Link>
+        </div>
+      ))}
+    </div>
+  );
 }
